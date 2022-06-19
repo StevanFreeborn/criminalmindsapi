58f3c0e93061fd05e60e52ddab5190ec4b396e16
StevanFreeborn <sfreeborn7@gmail.com>
2022-06-19 16:42:02 -0500

    include changes

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,19 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using server.Persistence;
+using server.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("MongoDBSettings"));
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp => 
+    sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+
+builder.Services.AddSingleton<IDbContext, DbContext>();
+
+builder.Services.AddScoped<ISeasonRepository, SeasonRepository>();
 
 builder.Services.AddControllers();
 
@@ -7,11 +22,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
