c5903ad901ee71ed2eaf545048d852560aa66982
StevanFreeborn <sfreeborn7@gmail.com>
2022-06-24 14:25:28 -0500

    tighten up program.cs and add optionality to seed data however

--- server/Program.cs
+++ server/Program.cs
@@ -1,29 +1,17 @@
 using server.Persistence.Seed;
 using server.Setup;
 
-if (args.Length == 2 && args[0].ToLower() == "seed")
+if (args.Length > 0 && args[0].ToLower() == "seed")
 {
     var seeder = new Seeder();
 
-    if (args[1].ToLower() == "seasons")
-    {
-        await seeder.SeedSeasonsAsync();
-    }
+    if (args.Contains("seasons")) await seeder.SeedSeasonsAsync();
 
-    if (args[1].ToLower() == "episodes")
-    {
-        await seeder.SeedEpisodesAsync();
-    }
+    if (args.Contains("episodes")) await seeder.SeedEpisodesAsync();
 
-    if (args[1].ToLower() == "quotes")
-    {
-        await seeder.SeedQuotesAsync();
-    }
+    if (args.Contains("quotes")) await seeder.SeedQuotesAsync();
 
-    if (args[1].ToLower() == "characters")
-    {
-        await seeder.SeedCharactersAsync();
-    }
+    if (args.Contains("characters")) await seeder.SeedCharactersAsync();
 }
 else
 {
