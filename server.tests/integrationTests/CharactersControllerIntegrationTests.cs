6d94582686253571ca455d4bccb336f6b143a181
StevanFreeborn <sfreeborn7@gmail.com>
2022-07-05 14:38:00 -0500

    added test project and put integration and unit tests under single test project

---
+++ server.tests/integrationTests/CharactersControllerIntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace server.tests.integrationTests
+{
+    public class CharactersControllerIntegrationTests 
+    {
+        private readonly HttpClient _client;
+
+        public CharactersControllerIntegrationTests()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            _client = webAppFactory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task GetCharacters_RetrievesAllCharacters_Returns200StatusCodeWithCharacters()
+        {
+            var response = await _client.GetAsync("/api/characters");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ server.tests/unitTests/CharacterControllerUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using server.Controllers.v1;
+using server.Persistence.Repositories;
+
+namespace server.tests.unitTests
+{
+    public class CharacterControllerUnitTests
+    {
+        private readonly Mock<ICharacterRepository> _mockRepo;
+        private readonly CharactersController _controller;
+
+        public CharacterControllerUnitTests()
+        {
+            _mockRepo = new Mock<ICharacterRepository>();
+            _controller = new CharactersController(_mockRepo.Object);
+        }
+    }
+}
